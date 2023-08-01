4e61cfcdb2a883598371b7c5124509a09fb8809f
Lucas Rosa <lrosa@keeta.com>
2023-08-02 00:18:54 +0100

    init commit

---
+++ dist/types.d.ts
@@ -0,0 +1,11 @@
+import { MouseEventHandler } from 'react';
+
+interface ButtonProps {
+    text?: string;
+    primary?: boolean;
+    disabled?: boolean;
+    size?: "small" | "medium" | "large";
+    onClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export { ButtonProps };
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,5 @@
+const Button: React.FC = () => {
+  return <div />;
+}
+
+export default Button;
